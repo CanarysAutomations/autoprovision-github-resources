e8fac31ec95332bf6391f7606a9d40832ed369ef
grenston.george.external <grenston.george.external@zf.com>
2021-09-16 18:29:59 +0530

    added title

--- git_autocreate.py
+++ git_autocreate.py
@@ -1,26 +1,26 @@
+'''
+The tool will communicate with the repositories and organizations of GitHub. The exe will be available for auto provision of GitHub resources with each release. 
+This tool is intended to restrict manual labour as much as possible. The tool with the help of few inputs will:
+    1. Create a repository - An empty repository will be created
+    2. Repository Access - The teams will be given access to the created repository based on the csv file input
+    3. Project - An empty project will be created for the repository with the Columns defined by the user.
+'''
+
+from modules.title import showTitle
 from modules.initialise import initialiseInputs
 from modules.createRepo import createRepo
 from modules.teamAccess import provideTeamAccess
 from modules.createProject import createProject
 from sys import argv
 
-print("   _____ _ _   _   _       _            ___        _              _____                _     " )
-print("  |  __ (_) | | | | |     | |          / _ \      | |            /  __ \              | |    ")
-print("  | |  \/_| |_| |_| |_   _| |__ ______/ /_\ \_   _| |_ ___ ______| /  \/_ __ ___  __ _| |_ ___")
-print("  | | __| | __|  _  | | | | '_ \______|  _  | | | | __/ _ \______| |   | '__/ _ \/ _` | __/ _ \\")
-print("  | |_\ \ | |_| | | | |_| | |_) |     | | | | |_| | || (_) |     | \__/\ | |  __/ (_| | ||  __/")
-print("   \____/_|\__\_| |_/\__,_|_.__/      \_| |_/\__,_|\__\___/       \____/_|  \___|\__,_|\__\___|")
-print()
-
 argumentList = argv[1:]
 initialisedValues = initialiseInputs(argumentList)
 inputObject, head, csvReader, type = initialisedValues['inputObject'], initialisedValues['head'], initialisedValues['csvReader'], initialisedValues['type']
+
+showTitle()
 createRepo(inputObject, head, type)
 provideTeamAccess(inputObject, head, csvReader)
 if not inputObject.ProjectName or not inputObject.Columns:
     print("\nProject Name or Columns not Specified. Skipping Project Creation\n")
 else:
-    createProject(inputObject, head)
-
-
-        
+    createProject(inputObject, head)     
