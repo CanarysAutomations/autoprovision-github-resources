b5a99c37f45d82f0b6a5a963449e12735c31499d
grenston.george.external <grenston.george.external@zf.com>
2021-09-16 17:45:36 +0530

    getopt issue

--- git_autocreate.py
+++ git_autocreate.py
@@ -15,136 +15,109 @@ print("  | | __| | __|  _  | | | | '_ \______|  _  | | | | __/ _ \______| |   | 
 print("  | |_\ \ | |_| | | | |_| | |_) |     | | | | |_| | || (_) |     | \__/\ | |  __/ (_| | ||  __/")
 print("   \____/_|\__\_| |_/\__,_|_.__/      \_| |_/\__,_|\__\___/       \____/_|  \___|\__,_|\__\___|")
 print()
+class inputClass:
+    def __init__ (self, argumentList=None):
+        if not argumentList:
+            self.PATToken = getpass('GitHub Token: ')
+            self.Organization = input('GitHub Organization: ')
+            self.RepoName = input('Repository Name: ')
+            self.Repository_Visibility = input('Repository Visibility: ')
+            self.RepoDescription = input('Add the Repository Description: ')
+            self.ProjectName = input('Project to be created for the Repository. Press ENTER if not required: ')
+            if self.ProjectName != "":
+                self.Columns = input('Project Column Names to be Created: ')
+            else:
+                self.Columns = ""
+            self.CsvSource = input('CSV Source (Use / for path separator): ')
+        else:           
+            if len(argumentList) == 16:
+                options = "t:o:r:v:d:p:c:f:"
+                long_options = ["Token =", "Organization =","Repository =", "Visibility =","Description =","Project =","Columns =","File ="]
+                try:
+                    arguments = getopt(argumentList, options, long_options)
+                    for currentArgument, currentValue in arguments:
+                        if currentArgument in ("-p", "--Project "):
+                            self.ProjectName = currentValue
+                        elif currentArgument in ("-c", "--Columns "):
+                            self.Columns = currentValue
+                except getopt.error as err:
+                    print (str(err))
+            else:
+                options = "t:o:r:v:d:f:"
+                long_options = ["Token =", "Organization =","Repository =", "Visibility =","Description =","File ="]
+                self.Columns = None
+                self.ProjectName = None
+            try:
+                arguments = getopt(argumentList, options, long_options)
+                for currentArgument, currentValue in arguments:
+                    if currentArgument in ("-t", "--Token "):
+                        self.PATToken = currentValue                
+                    elif currentArgument in ("-o", "--Organization "):
+                        self.Organization = currentValue               
+                    elif currentArgument in ("-r", "--Repository "):
+                        self.RepoName = currentValue
+                    elif currentArgument in ("-v", "--Visibility "):
+                        self.Repository_Visibility = currentValue
+                    elif currentArgument in ("-d", "--Description "):
+                        self.RepoDescription = currentValue
+                    elif currentArgument in ("-f", "--File "):
+                        self.CsvSource = currentValue
+            except getopt.error as err:
+                print (str(err))
+
+
+def initialiseInputs(argumentList):
+    if len(argumentList) == 0:
+        inputObject = inputClass()
+    elif len(argumentList) != 16 and len(argumentList) != 12:
+        print("Refer README for unattended usage. Supports only 6 or 8 arguments.")
+        choice = input("Enter \"Y\" to input values manually: ")
+        if (choice.upper() == "Y"):
+            inputObject = inputClass()
+        else:
+            exit("Aborting.......")
+    elif len(argumentList) == 16:
+        if set(["-t","-o","-r","-v","-d","-p","-c","-f"]) <= set(argumentList) or set(["--Token ", "--Organization ","--Repository ", "--Visibility ","--Description ","--Project ","--Columns ","--File "]) <= set(argumentList):
+            inputObject = inputClass(argumentList)
+        else:
+            print("Refer README for unattended usage. Proper arguments not provided.")
+            choice = input("Enter \"Y\" to input values manually: ")
+            if (choice.upper() == "Y"):
+                inputObject = inputClass()
+            else:
+                exit("Aborting.......")
+    else:
+        if set(['-t','-o','-r','-v','-d','-f']) <= set(argumentList) or set(["--Token ", "--Organization ","--Repository ", "--Visibility ","--Description ","--File "]) <= set(argumentList):
+            inputObject = inputClass(argumentList) 
+        else: 
+            print("Refer README for unattended usage. Proper arguments not provided.")
+            choice = input("Enter \"Y\" to input values manually: ")
+            if (choice.upper() == "Y"):
+                inputObject = inputClass()
+            else:
+                exit("Aborting.......")
+            
+    csvReader = DictReader(open(inputObject.CsvSource))
+    if inputObject.Repository_Visibility == 'public':
+        type = 'public'
+    elif inputObject.Repository_Visibility == 'internal':
+        type = 'internal'
+    else:
+        type='private'
+
+    head = {
+        'Authorization' : 'Bearer ' + inputObject.PATToken,
+        'Accept' : 'application/vnd.github.v3+json;application/vnd.github.nebula-preview+json;application/vnd.github.inertia-preview+json'
+    }
+    returnValues = {'inputObject':inputObject,'head':head,'csvReader':csvReader,'type':type}
+    return returnValues
 
 argumentList = argv[1:]
-if len(argumentList) == 0:
-    PATToken = getpass('GitHub Token: ')
-    Organization = input('GitHub Organization: ')
-    RepoName = input('Repository Name: ')
-    Repository_Visibility = input('Repository Visibility: ')
-    RepoDescription = input('Add the Repository Description: ')
-    ProjectName = input('Project to be created for the Repository. Press ENTER if not required: ')
-    if ProjectName != "":
-        Columns = input('Project Column Names to be Created: ')
-    else:
-        Columns = ""
-    CsvSource = input('CSV Source (Use / for path separator): ')    
-elif len(argumentList) != 16 and len(argumentList) != 12:
-    print("Refer README for unattended usage. Supports only 6 or 8 arguments.")
-    choice = input("Enter \"Y\" to input values manually: ")
-    if (choice.upper() == "Y"):
-        PATToken = getpass('GitHub Token: ')
-        Organization = input('GitHub Organization: ')
-        RepoName = input('Repository Name: ')
-        Repository_Visibility = input('Repository Visibility: ')
-        RepoDescription = input('Add the Repository Description: ')
-        ProjectName = input('Project to be created for the Repository. Press ENTER if not required: ')
-        if ProjectName != "":
-            Columns = input('Project Column Names to be Created: ')
-        else:
-            Columns = ""
-        CsvSource = input('CSV Source (Use / for path separator): ')
-    else:
-        exit("Aborting.......")
-elif len(argumentList) == 16:
-    if set(["-t","-o","-r","-v","-d","-p","-c","-f"]) <= set(argumentList) or set(["--Token ", "--Organization ","--Repository ", "--Visibility ","--Description ","--Project ","--Columns ","--File "]) <= set(argumentList):
-        options = "t:o:r:v:d:p:c:f:"
-        long_options = ["Token =", "Organization =","Repository =", "Visibility =","Description =","Project =","Columns =","File ="]
-        try:
-            arguments, values = getopt(argumentList, options, long_options)
-            for currentArgument, currentValue in arguments:
-                if currentArgument in ("-t", "--Token "):
-                    PATToken = currentValue                
-                elif currentArgument in ("-o", "--Organization "):
-                    Organization = currentValue               
-                elif currentArgument in ("-r", "--Repository "):
-                    RepoName = currentValue
-                elif currentArgument in ("-v", "--Visibility "):
-                    Repository_Visibility = currentValue
-                elif currentArgument in ("-d", "--Description "):
-                    RepoDescription = currentValue
-                elif currentArgument in ("-p", "--Project "):
-                    ProjectName = currentValue
-                elif currentArgument in ("-c", "--Columns "):
-                    Columns = currentValue
-                elif currentArgument in ("-f", "--File "):
-                    CsvSource = currentValue
-        except getopt.error as err:
-            print (str(err))
-    else:
-        print("Refer README for unattended usage. Proper arguments not provided.")
-        choice = input("Enter \"Y\" to input values manually: ")
-        if (choice.upper() == "Y"):
-            PATToken = getpass('GitHub Token: ')
-            Organization = input('GitHub Organization: ')
-            RepoName = input('Repository Name: ')
-            Repository_Visibility = input('Repository Visibility: ')
-            RepoDescription = input('Add the Repository Description: ')
-            ProjectName = input('Project to be created for the Repository. Press ENTER if not required: ')
-            if ProjectName != "":
-                Columns = input('Project Column Names to be Created: ')
-            else:
-                Columns =""
-            CsvSource = input('CSV Source (Use / for path separator): ')
-        else:
-            exit("Aborting.......")
-else:
-    if set(['-t','-o','-r','-v','-d','-f']) <= set(argumentList) or set(["--Token ", "--Organization ","--Repository ", "--Visibility ","--Description ","--File "]) <= set(argumentList):
-        options = "t:o:r:v:d:f:"
-        long_options = ["Token =", "Organization =","Repository =", "Visibility =","Description =","File ="]
-        Columns = None
-        ProjectName = None
-        try:
-            arguments, values = getopt(argumentList, options, long_options)
-            for currentArgument, currentValue in arguments:
-                if currentArgument in ("-t", "--Token "):
-                    PATToken = currentValue              
-                elif currentArgument in ("-o", "--Organization "):
-                    Organization = currentValue               
-                elif currentArgument in ("-r", "--Repository "):
-                    RepoName = currentValue
-                elif currentArgument in ("-v", "--Visibility "):
-                    Repository_Visibility = currentValue
-                elif currentArgument in ("-d", "--Description "):
-                    RepoDescription = currentValue
-                elif currentArgument in ("-f", "--File "):
-                    CsvSource = currentValue
-        except getopt.error as err:
-            print (str(err)) 
-    else: 
-        print("Refer README for unattended usage. Proper arguments not provided.")
-        choice = input("Enter \"Y\" to input values manually: ")
-        if (choice.upper() == "Y"):
-            PATToken = getpass('GitHub Token: ')
-            Organization = input('GitHub Organization: ')
-            RepoName = input('Repository Name: ')
-            Repository_Visibility = input('Repository Visibility: ')
-            RepoDescription = input('Add the Repository Description: ')
-            ProjectName = input('Project to be created for the Repository. Press ENTER if not required: ')
-            if ProjectName != "":
-                Columns = input('Project Column Names to be Created: ')
-            else:
-                Columns =""
-            CsvSource = input('CSV Source (Use / for path separator): ')
-        else:
-            exit("Aborting.......")
-           
-csvReader = DictReader(open(CsvSource))
-if Repository_Visibility == 'public':
-    type = 'public'
-elif Repository_Visibility == 'internal':
-    type = 'internal'
-else:
-    type='private'
-
-head = {
-    'Authorization' : 'Bearer ' + PATToken,
-    'Accept' : 'application/vnd.github.v3+json;application/vnd.github.nebula-preview+json;application/vnd.github.inertia-preview+json'
-}
-
+initialisedValues = initialiseInputs(argumentList)
+inputObject, head, csvReader, type = initialisedValues['inputObject'], initialisedValues['head'], initialisedValues['csvReader'], initialisedValues['type']
 RepoDetails = {
-    'name':RepoName,
-    'description':RepoDescription,
+    'name':inputObject.RepoName,
+    'description':inputObject.RepoDescription,
     'visibility':type
 }
 
@@ -152,7 +125,7 @@ print()
 print( "Create repository in organization")
 print( "=================================")
 
-create_repo_req_url = 'https://api.github.com/orgs/' + Organization + '/repos'
+create_repo_req_url = 'https://api.github.com/orgs/' + inputObject.Organization + '/repos'
 try:
     create_repo_response = post(url=create_repo_req_url, headers=head, data=dumps(RepoDetails))
     repository_name = create_repo_response.json()['name']
@@ -173,33 +146,33 @@ try:
         repobody = {
             'permission' : repopermission
         }
-        give_team_access_req_url = 'https://api.github.com/orgs/' + Organization + '/teams/' + teamname.lower() + '/repos/' + Organization + '/' + RepoName
+        give_team_access_req_url = 'https://api.github.com/orgs/' + inputObject.Organization + '/teams/' + teamname.lower() + '/repos/' + inputObject.Organization + '/' + inputObject.RepoName
         give_team_access_response = put(give_team_access_req_url, headers=head, data=dumps(repobody))
         team_newname = teamname.upper()
-        repo_newname = RepoName.upper()
+        repo_newname = inputObject.RepoName.upper()
         print('Team',team_newname,'has been given',repopermission,'to',repo_newname)
 except:
     print(colored("Unable to provide access for {}".format(teamname.upper()),'red'))
 
-if not ProjectName or not Columns:
+if not inputObject.ProjectName or not inputObject.Columns:
     print("\nProject Name or Columns not Specified. Skipping Project Creation\n")
 else:
     projectdetails = {
-        'name' : ProjectName,
-        'body': 'created for repo ' + RepoName
+        'name' : inputObject.ProjectName,
+        'body': 'created for repo ' + inputObject.RepoName
     }
 
     try:
         print()
         print("Create a project for the repository")
         print("===================================")
-        create_project_req_url = 'https://api.github.com/repos/' + Organization + '/' + RepoName + '/projects'
+        create_project_req_url = 'https://api.github.com/repos/' + inputObject.Organization + '/' + inputObject.RepoName + '/projects'
         create_project_response = post(url=create_project_req_url, headers=head, data=dumps(projectdetails))
-        print(ProjectName,"Project is Created")
+        print(inputObject.ProjectName,"Project is Created")
     except:
         print(colored(create_project_response.json(),'red'))
     else:
-        ColumnNames = Columns.split(',')
+        ColumnNames = inputObject.Columns.split(',')
         projectID = create_project_response.json()['id']
         create_columns_req_url = 'https://api.github.com/projects/' + str(projectID) + '/columns'
         try:
